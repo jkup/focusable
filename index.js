f633f9890551c7cb3b6b2a21e5b00e4e3f1b0527
Scott Vinkle <svinkle@users.noreply.github.com>
2018-03-19 22:37:49 -0400

    Added summary element
    This adds the [`summary`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/summary) element which is the focusable control for the native [`details`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details) element.

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls]'
+module.exports = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary'
