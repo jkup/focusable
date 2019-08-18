ce83bb7eec5a8a6305b61cbeeffc7bac7d62efac
Limon Monte <limon.monte@gmail.com>
2019-08-18 18:28:40 +0300

    support [tabindex^="\d"]

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary'
+function tabindexes () {
+  var result = ''
+  for (var i = 0; i <= 9; i++) {
+    result += ', [tabindex^="' + i + '"]'
+  }
+  return result
+}
+
+module.exports = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, ' + tabindexes()
