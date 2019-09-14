2350a73a2ae1a3607a9dc79b116d1f25550a2fa4
Limon Monte <limon.monte@gmail.com>
2019-09-14 08:51:42 +0300

    use simple string

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function tabindexes () {
-  var result = ''
-  for (var i = 0; i <= 9; i++) {
-    result += ', [tabindex^="' + i + '"]'
-  }
-  return result
-}
-
-module.exports = 'a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, '  + tabindexes()
+module.exports = 'a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, [tabindex^="0"], [tabindex^="1"], [tabindex^="2"], [tabindex^="3"], [tabindex^="4"], [tabindex^="5"], [tabindex^="6"], [tabindex^="7"], [tabindex^="8"], [tabindex^="9"]'
